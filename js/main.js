c31e712878ee7375f1b9f38ea36fd5ce94f5a2ec
Madhuran Thiagarajah <madhuran.v@gmail.com>
2019-12-26 20:31:13 -0500

    major rehaul

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Responsive Nav
+$(function() {
+    menu = $('nav ul');
+  
+    $('#openup').on('click', function(e) {
+      e.preventDefault();
+      menu.slideToggle();
+    });
+  
+    $(window).resize(function() {
+      var w = $(this).width();
+      if (w > 480 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+      }
+    });
+  
+    $('nav li').on('click', function(e) {
+      var w = $(window).width();
+      if (w < 480) {
+        menu.slideToggle();
+      }
+    });
+    $('.open-menu').height($(window).height());
+  });
+  
+  window.onscroll = () => {
+    const nav = document.querySelector('#navbar');
+    if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+  };
