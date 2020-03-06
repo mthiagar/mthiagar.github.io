0aefdfc51b669d426bc90170e2c2514c6d28bfbb
Madhuran Thiagarajah <madhuran.v@gmail.com>
2020-03-05 21:09:04 -0500

    Various changes.

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.static('css'));
+app.use(express.static('js'));
+
+app.get('/', (req, res) => res.send('index'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
